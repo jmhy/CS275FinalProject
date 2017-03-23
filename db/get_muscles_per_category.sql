6941cfbde6597c695cf2c77e01ee153d3438a6d5
Joe Haggerty <joemhaggerty@gmail.com>
2017-03-22 22:27:46 -0400

    Fixed bug getting muscles for body part

--- db/get_muscles_per_category.sql
+++ db/get_muscles_per_category.sql
@@ -1,7 +1,8 @@
-set @category='arms';
+set @category='front_torso';
 select fk_muscle1_id, m1.name mn1, m1.img_filepath mi1, fk_muscle2_id, m2.name mn2, m2.img_filepath mi2, fk_muscle3_id, m3.name mn3, m3.img_filepath mi3, fk_muscle4_id, m4.name mn4, m4.img_filepath mi4
 from muscle_categories
-inner join muscles m1 on (fk_muscle1_id=m1.m_id) and muscle_categories.name=@category
-inner join muscles m2 on (fk_muscle2_id=m2.m_id) and muscle_categories.name=@category
-inner join muscles m3 on (fk_muscle3_id=m3.m_id) and muscle_categories.name=@category
-inner join muscles m4 on (fk_muscle4_id=m4.m_id) and muscle_categories.name=@category
+left join muscles m1 on (fk_muscle1_id=m1.m_id)
+left join muscles m2 on (fk_muscle2_id=m2.m_id)
+left join muscles m3 on (fk_muscle3_id=m3.m_id)
+left join muscles m4 on (fk_muscle4_id=m4.m_id)
+where muscle_categories.name=@category
