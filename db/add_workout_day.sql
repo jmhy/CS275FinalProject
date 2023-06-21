0fdbd0d3bf1c3928fc77114c95267b6e95e8907c
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-21 08:51:49 +0800

    Delete db directory

--- db/add_workout_day.sql
+++
@@ -1,6 +0,0 @@
-insert into workout_day
-(notes, fk_ex1_id, e1_sets, e1_reps, e1_rest, fk_ex2_id, e2_sets, e2_reps, e2_rest, fk_ex3_id, e3_sets, e3_reps, e3_rest, fk_ex4_id, e4_sets, e4_reps, e4_rest, fk_ex5_id, e5_sets, e5_dur)
-values
-('day 1 mens beginner',1,3,10,'00:01:00',2,3,10,'00:01:00',3,3,10,'00:01:00',4,3,10,'00:01:00',5,1,'00:20:00'),
-('day 2 mens beginner',6,3,10,'00:01:00',7,3,10,'00:01:00',8,3,10,'00:01:00',9,3,10,'00:01:00',5,1,'00:10:00'),
-('day 3 mens beginner',10,3,10,'00:01:00',11,3,10,'00:01:00',12,3,10,'00:01:00',13,3,10,'00:01:00',5,1,'00:20:00');
