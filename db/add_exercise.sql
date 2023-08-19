65113cf123f6011c0b45f0ae442683b77e10fe69
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-19 12:49:12 +0800

    Delete db directory

--- db/add_exercise.sql
+++
@@ -1,8 +0,0 @@
-insert into exercises
-(name, instructions, fk_muscle1_id, fk_muscle2_id, fk_muscle3_id)
-values
-('Bench Press', '<p><b>Step 1:</b> Lie on your back on a flat bench. Lift the bar off the rack and hold it straight over you, keeping your arms locked. This is the starting position.</p>
-<p><b>Step 2:</b> Next, inhale and bring the barbell down in a slow and controlled manner until it reaches your mid-chest.</p>
-<p><b>Step 3:</b> Pause briefly before raising the barbell back to your starting position as you exhale. Your focus should be on using your chest muscles to move the bar. Lock your arms at the top of the movement and squeeze your chest before slowly bringing the barbell down again. This step should take twice as long raising the weight to get the maximum benefit.</p>
-<p><b>Step 4:</b> Repeat the movement for the desired number of repetitions.</p>
-<p><b>Step 5:</b> The final step in the exercise is to place the barbell on the rack.</p>', 4, 12, 14);
--- db/get_exercises_in_plan.sql
+++
@@ -1,7 +0,0 @@
-#gets all columns for all exercises within a given plan, example is women's beginner
-select distinct exercises.*
-from exercises, workout_day, workout_plan
-where plan_id = 2
-and (workout_day_id = fk_workout_sun or workout_day_id = fk_workout_mon or workout_day_id = fk_workout_tue or workout_day_id = fk_workout_wed or workout_day_id = fk_workout_thu or workout_day_id = fk_workout_fri or workout_day_id = fk_workout_sat)
-and (fk_ex1_id = exercise_id or fk_ex2_id = exercise_id or fk_ex3_id = exercise_id or fk_ex4_id = exercise_id or fk_ex5_id = exercise_id or fk_ex6_id = exercise_id or fk_ex7_id = exercise_id or fk_ex8_id = exercise_id or fk_ex9_id = exercise_id or fk_ex10_id = exercise_id)
-order by name
